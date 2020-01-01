e4e085e0dcc126bbb766af7d95ea3143b001585d
Mac <sudiptoroy@sudipto.local>
2020-01-01 16:05:22 +0600

    Initial Commit

---
+++ TableViewTest/VideoListScreen.swift
@@ -0,0 +1,30 @@
+//
+//  VideoListScreen.swift
+//  TableViewTest
+//
+//  Created by Sudipto Roy on 1/1/20.
+//  Copyright © 2020 Code-X Systems. All rights reserved.
+//
+
+import UIKit
+
+class VideoListScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
